6233b9c8d217fe511c6a5102b154ea7fc6df069a
DNason1999 <50815684+DNason1999@users.noreply.github.com>
2020-01-07 15:43:28 -0600

    Update StrainAPI.py

--- StrainAPI/StrainAPI.py
+++ StrainAPI/StrainAPI.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 
 class Strainer():
     def __init__(self):
-        self.filename = "Dummy_linear_regression.pkl"
+        self.filename = "dummy_linear_regression.pkl"
         self.model = pickle.load(open(self.filename, 'rb'))
 
     def get_strain(self, data):
