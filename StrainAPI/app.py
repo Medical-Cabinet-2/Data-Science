b2c2712cc91962cbff335f0f42a56472adfe9f65
DNason1999 <50815684+DNason1999@users.noreply.github.com>
2020-01-08 14:34:15 -0600

    added API Documentation

--- StrainAPI/app.py
+++ StrainAPI/app.py
@@ -1,32 +1,22 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import json
 from .StrainAPI import Strainer
 
 def create_app():
     app = Flask(__name__)
 
-    api = Strainer()
+    #api = Strainer()
 
 
     @app.route('/')
     def root():
-        """Default route, doesnt do anything"""
-        msg = """
-        <pre>
-        Default route for the api\n
-
-        Use /search to determine a strain for the user
-        pass a json object with the data and it will return
-        a json object with the result.
-        As of 10:53 AM lambda time, the /search function
-        will accept an input in the form of a json payload
-        in the form of {'input': (int)} and return a json
-        payload in the form of {'id': (int)+1}. Integration
-        of the actual ML Algorithm is in progress and should
-        be completed by the end of the day (01/08/2020)
-        </pre>
-        """
-        return msg
+        """Default route, renders documentation.html"""
+        return render_template('documentation.html')
+    
+    @app.route('/search.html')
+    def s():
+        """Renders the search function documentation"""
+        return render_template('search.html')
     
     @app.route('/search', methods=['POST'])
     def search():
