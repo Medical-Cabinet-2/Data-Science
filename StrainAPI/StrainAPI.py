9c872baa13b798845bf39fffee25216ee5bc580b
DNason1999 <50815684+DNason1999@users.noreply.github.com>
2020-01-08 12:31:44 -0600

    Update StrainAPI.py

--- StrainAPI/StrainAPI.py
+++ StrainAPI/StrainAPI.py
@@ -5,6 +5,8 @@ import os
 
 class Strainer():
     def __init__(self):
+        print(os.getcwd())
+        print(os.listdir())
         self.filename = '../StrainAPI/models/dummy_linear_regression.pkl'
         self.model = pickle.load(open(self.filename, 'rb'))
 
