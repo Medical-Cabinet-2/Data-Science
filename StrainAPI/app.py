140be7bd02f66da190723309ff9b051329ecd48a
DNason1999 <50815684+DNason1999@users.noreply.github.com>
2020-01-09 21:16:24 -0600

    changes

--- StrainAPI/app.py
+++ StrainAPI/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, render_template
 import json
 from StrainAPI.nlp_model import Predictor
-import spacy
 
 def create_app():
     app = Flask(__name__)
@@ -23,35 +22,23 @@ def create_app():
         """Renders the test file"""
         return render_template('test.html')
     
-    @app.route('/search', methods=['POST'])
+    @app.route('/search', methods=['GET'])
     def search():
         """Useful route, calls the get_strain method"""
         # Accesses the json payload from the http request
-        data = json.loads(request.get_json())
 
-        types = data['type']
-        flavor= data['flavor']
-        effects= data['effects']
-        description = data['desc']
-        in_size = 3
+        types = request.args.get('type')
+        flavor= request.args.get('flavor')
+        effects = request.args.get('effects')
+        description = request.args.get('desc')
+        in_size = int(request.args.get('size'))
 
         user_text = types+" "+flavor+" "+effects+" "+description
 
         # Generate the result from the machine learning api
         result = api.predict(user_input_text=user_text, size= in_size)
+        print(result)
 
         return json.dumps({'id':result[0]})
-
-    def get_lemmas(self, text):
-        """Return the Lemmas"""
-        nlp = spacy.load("en_core_web_md")
-        lemmas = []
-        doc = nlp(text)
-    
-        for token in doc: 
-            if ((token.is_stop == False) and (token.is_punct == False)) and (token.pos_!= 'PRON'):
-                lemmas.append(token.lemma_)
-    
-        return lemmas
-
+            
     return app
--- StrainAPI/nlp_model.py
+++ StrainAPI/nlp_model.py
@@ -5,35 +5,43 @@ import pandas as pd
 import numpy as np
 import spacy
 
-from spacy.tokenizer import Tokenizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.neighbors import NearestNeighbors
 
 ###################
 ##BUILD PREDICTOR##
 ###################
+
 class Predictor():
-    def __init__(self):
+    def __init__(self):    
         # Load in the pickled model
-        self.nn = pickle.load(open(os.getcwd()+"/StrainAPI/models/nn_1.pkl", "rb"))
-        self.tfidf = pickle.load(open(os.getcwd()+"/StrainAPI/models/tfidf_1.pkl", "rb"))
+        self.nn = pickle.load(open("./models/nn_1.pkl", "rb"))
+        self.tfidf = pickle.load(open("./models/tfidf_1.pkl", "rb"))
+        # self.pars = pickle.load(open("./models/pars.pkl", "rb"))
+        # self.vocab = pickle.load(open("./models/vocab.pkl", "rb"))
+        # self.idf = pickle.load(open("./models/idf.pkl", "rb"))
+        # self.pars['tokenizer']=get_lemmas
+        # self.tfidf = TfidfVectorizer(self.pars)
+        # self.tfidf.vocabulary_ = self.vocab
+        # self.tfidf.idf_ = self.idf
+        #self.nn = pickle.load(open(os.getcwd()+"/StrainAPI/models/nn_1.pkl", "rb"))
+        #self.tfidf = pickle.load(open(os.getcwd()+"/StrainAPI/models/tfidf_1.pkl", "rb"))
 
     def predict(self,user_input_text,size=5):
         # Create vector from request string
-        request_vec = self.tfidf.transform(user_input_text)
+        request_vec = self.tfidf.transform([user_input_text])
         # Use knn model to calculate the top n strains
         # The recommendations are the top n nearest points (vectors) to the
         # vectorized request, based on the vectorized dataset (vocab).
         strain_ids = self.nn.kneighbors(request_vec.todense(), n_neighbors=size)[1][0]
         return strain_ids
 
-    def get_lemmas(text):
-        """Return the Lemmas"""
-        lemmas = []
-        doc = nlp(text)
-    
-        for token in doc: 
-            if ((token.is_stop == False) and (token.is_punct == False)) and (token.pos_ != 'PRON'):
-                lemmas.append(token.lemma_)
-    
-        return lemmas
+def get_lemmas(text):
+    """Return the Lemmas"""
+    lemmas = []
+    doc = nlp(text)
+
+    for token in doc: 
+        if ((token.is_stop == False) and (token.is_punct == False)) and (token.pos_!= 'PRON'):
+            lemmas.append(token.lemma_)
+
+    return lemmas
